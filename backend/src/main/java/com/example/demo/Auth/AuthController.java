3fe6b41b570add55d8166c79b09e01da109ae250
Angssang <codeunsang30@gmail.com>
2025-04-21 17:29:38 +0900

    a

--- backend/src/main/java/com/example/demo/Auth/AuthController.java
+++ backend/src/main/java/com/example/demo/Auth/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.demo.Auth;
 
 import com.example.demo.Auth.DTO.SignUpRequestDTO;
+import com.example.demo.Auth.DTO.SignUpResponseDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +13,16 @@ public class AuthController {
     AuthService authService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity signUp(@Valid @RequestBody SignUpRequestDTO signUpRequestDTO) {
-        System.out.println("username: "+ signUpRequestDTO.getUsername());
-        System.out.println("displayname: "+ signUpRequestDTO.getDisplayName());
-        System.out.println("email: "+ signUpRequestDTO.getEmail());
-        System.out.println("password: "+ signUpRequestDTO.getPassword());
+    public ResponseEntity signUp(@Valid @RequestBody SignUpRequestDTO request) {
+        SignUpResponseDTO response = new SignUpResponseDTO();
+
+        String username = request.getUsername();
+        String displayName = request.getDisplayName();
+        String password = request.getPassword();
+        String email = request.getEmail();
+
+        response.setUsername(username.matches(AuthRegExp.USERNAME.getPattern()) ? "a" : "");
+        authService.signUp(request);
         return null;
 //        return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
